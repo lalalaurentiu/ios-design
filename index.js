a3cca5276831b11be38b047f4ff32ea06c9cb03d
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2022-07-20 20:37:22 +0300

    resolve loading screen bug

--- index.js
+++ index.js
@@ -82,18 +82,33 @@ if(window.innerWidth > 1024){
     loadingElements.style.height = `${screenHeight}px`
 
     // header desktop
-    header.style.width = `calc(${screenWidth}px - 2px)`
+    // header.style.width = `calc(${screenWidth}px - 2px)`
     header.innerHTML = ""
 
     let leftheaderapp = document.createElement("div")
     header.append(leftheaderapp)
 
-
     loading(startLoading,screenWidth, loadingElements, loadingScreen, progresBarElement, header)
 
     class HeaderApp{
         app = document.createElement("div")
+        constructor (headerContainer, title){
+            this.headerContainer = headerContainer
+            this.title = title
+        }
+        containerSelf(){
+            this.app.setAttribute("style", `
+                width:20px;
+            `)
+            this.app.innerHTML = this.title
+            this.headerContainer.append(this.app)
+        }
     }
+
+    // desktop header apps
+    const mac = new HeaderApp(leftheaderapp,
+                                `<svg xmlns="http://www.w3.org/2000/svg" fill="white" data-name="Layer 1" viewBox="0 0 24 24"><path d="M14.94,5.19A4.38,4.38,0,0,0,16,2,4.44,4.44,0,0,0,13,3.52,4.17,4.17,0,0,0,12,6.61,3.69,3.69,0,0,0,14.94,5.19Zm2.52,7.44a4.51,4.51,0,0,1,2.16-3.81,4.66,4.66,0,0,0-3.66-2c-1.56-.16-3,.91-3.83.91s-2-.89-3.3-.87A4.92,4.92,0,0,0,4.69,9.39C2.93,12.45,4.24,17,6,19.47,6.8,20.68,7.8,22.05,9.12,22s1.75-.82,3.28-.82,2,.82,3.3.79,2.22-1.24,3.06-2.45a11,11,0,0,0,1.38-2.85A4.41,4.41,0,0,1,17.46,12.63Z"/></svg>`)
+    mac.containerSelf()
 }else {
     loading(startLoading,screenWidth, loadingElements, loadingScreen, progresBarElement)
 
